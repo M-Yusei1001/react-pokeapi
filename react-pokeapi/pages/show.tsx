d85d829e41307836d2014465d1449cc454a93f1d
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-09-08 13:52:20 +0000

    add components pokedata.tsx

---
+++ react-pokeapi/pages/show.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PokeData from "../components/pokedata";
+
+export default function ShowPokemon() {
+  return (
+    <div>
+      <PokeData pokedexId={1} />
+    </div>
+  );
+}
