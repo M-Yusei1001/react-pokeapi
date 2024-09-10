3563d56b0b3aab54054d6d94c5c1758010dd60a2
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-09-10 01:29:23 +0000

    fix hotreload

--- react-pokeapi-dev/next.config.mjs
+++ react-pokeapi-dev/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode:true,
+    webpack:(config,{dev})=>{
+        if(dev){
+            config.watchOptions = {
+                poll:1000,
+                aggregateTimeout:200,
+            };
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
