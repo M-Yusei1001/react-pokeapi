27fbf1f6909b39e911d248c3ef39e7538a67ab9d
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-09-21 03:22:27 +0000

    pokedata card

--- react-pokeapi-dev/app/_utils/pokedata.tsx
+++ react-pokeapi-dev/app/_utils/pokedata.tsx
@@ -4,13 +4,13 @@ import PokeName from "./pokename";
 
 export default function PokeData({ pokedexId }: { pokedexId: number }) {
   return (
-    <>
-      <div className="text-3xl">
-        <PokeName pokedexId={pokedexId} />
-      </div>
-      <div>
+    <div className="flex flex-col items-center w-80 rounded-2xl bg-white shadow-lg">
+      <div className="p-4">
         <PokeImage pokedexId={pokedexId} />
       </div>
-    </>
+      <div className="text-3xl font-bold p-4">
+        <PokeName pokedexId={pokedexId} />
+      </div>
+    </div>
   );
 }
--- react-pokeapi-dev/app/show/page.tsx
+++ react-pokeapi-dev/app/show/page.tsx
@@ -1,12 +1,20 @@
-"use client";
-
 import React from "react";
 import PokeData from "../_utils/pokedata";
 
+const pokeIds = [1, 5, 689, 10, 13, 123, 89, 90, 150, 432];
+
 export default function Home() {
   return (
-    <div>
-      <PokeData pokedexId={10} />
+    <div className="container mx-auto bg-slate-400">
+      <div className="flex flex-wrap flex-col justify-center items-center md:flex-row">
+        {pokeIds.map((pokeId) => {
+          return (
+            <div className="p-4" key={pokeId}>
+              <PokeData pokedexId={pokeId} key={pokeId} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
